fd40ce305c91458e20f205c689a8bfbeca248f57
S-Ivanov <s.v.ivanov@bk.ru>
2017-11-15 21:51:52 +0300

    Изменена кодировка на UTF-8

--- src/Driver.java
+++ src/Driver.java
@@ -4,11 +4,30 @@
  */
 public class Driver {
 
-    /**
-     * Конструктор
-     * 
-     */
-	public Driver(String lastName, String firstName, String middleName, String passportSeries, String passportNumber, String car) {
+	/**
+	 * Конструктор
+	 * 
+	 * @param lastName
+	 *            Фамилия
+	 * @param firstName
+	 *            Имя
+	 * @param middleName
+	 *            Отчество
+	 * @param passportSeries
+	 *            Серия паспорта
+	 * @param passportNumber
+	 *            Номер паспорта
+	 * @param car
+	 *            Марка и номер автомобиля
+	 * @param lastName
+	 *            фамилия
+	 */
+	public Driver(String lastName, String firstName, String middleName, String passportSeries, String passportNumber,
+			String car) {
+		
+//		if (lastName == null)
+//			throw new IllegalArgumentException("");
+		
 		this.lastName = lastName;
 		this.firstName = firstName;
 		this.middleName = middleName;
@@ -16,54 +35,54 @@ public class Driver {
 		this.passportNumber = passportNumber;
 		this.car = car;
 	}
-	
-    /**
-     * Фамилия
-     * 
-     */
-    public String getLastName() {
-        return lastName;
-    }
-	
-    /**
-     * Имя
-     * 
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-	
-    /**
-     * Отчество
-     * 
-     */
-    public String getMiddleName() {
-        return middleName;
-    }
-	
-    /**
-     * Серия паспорта
-     * 
-     */
-    public String getPassportSeries() {
-        return passportSeries;
-    }
-	
-    /**
-     * Номер паспорта
-     * 
-     */
-    public String getPassportNumber() {
-        return passportNumber;
-    }
-	
-    /**
-     * Марка и номер автомобиля
-     * 
-     */
-    public String getCar() {
-        return car;
-    }
-	
+
+	/**
+	 * Фамилия
+	 * 
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * Имя
+	 * 
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * Отчество
+	 * 
+	 */
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	/**
+	 * Серия паспорта
+	 * 
+	 */
+	public String getPassportSeries() {
+		return passportSeries;
+	}
+
+	/**
+	 * Номер паспорта
+	 * 
+	 */
+	public String getPassportNumber() {
+		return passportNumber;
+	}
+
+	/**
+	 * Марка и номер автомобиля
+	 * 
+	 */
+	public String getCar() {
+		return car;
+	}
+
 	String lastName, firstName, middleName, passportSeries, passportNumber, car;
 }
